232523cd2ff72f3155af06fc7d12c5f87e436c99
Roshani Deodatt Mishra <137196726+Roshani005@users.noreply.github.com>
2026-09-07 16:37:01 +0530

    Create stored procedure to load Bronze Layer data
    This stored procedure loads data into the 'bronze' schema from external CSV files, truncating existing tables and using BULK INSERT for data insertion. It includes error handling and prints load durations for each step.

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,153 @@
+/*
+=======================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+=======================================================================
+
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from
+    external CSV files.
+
+    It performs the following actions:
+        - Truncates the Bronze tables before loading new data.
+        - Uses the BULK INSERT command to load data from CSV files
+          into the Bronze tables.
+
+Parameters:
+    None.
+
+    This stored procedure does not accept any parameters or return
+    any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+
+=======================================================================
+*/
+
+create or alter procedure bronze.load_bronze as
+begin
+	declare @start_time datetime , @end_time datetime , @batch_start_time datetime , @batch_end_time datetime;
+	begin try
+		set @batch_start_time = getdate();
+		print '==================================================================';
+		print 'Loading Bronze Layer';
+		print '=================================================================='
+
+		print '------------------------------------------------------------------';
+		print 'Lading CRM Tables';
+		print '------------------------------------------------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.crm_cust_info';
+		truncate table bronze.crm_cust_info;
+
+		print '>> Inserting Table : bronze.crm_cust_info';
+		bulk insert bronze.crm_cust_info
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = getdate();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time,@end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.crm_prd_info';
+		truncate table bronze.crm_prd_info;
+
+		print '>> Inserting Table : bronze.crm_prd_info';
+		bulk insert bronze.crm_prd_info
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = GETDATE();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time, @end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.crm_sales_details';
+		truncate table bronze.crm_sales_details;
+
+		print '>> Inserting Table : bronze.crm_sales_details';
+		bulk insert bronze.crm_sales_details
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = GETDATE();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time, @end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		print '------------------------------------------------------------------';
+		print 'Lading ERP Tables';
+		print '------------------------------------------------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.erp_cust_az12';
+		truncate table bronze.erp_cust_az12;
+
+		print '>> Insterting Table : bronze.erp_cust_az12';
+		bulk insert bronze.erp_cust_az12
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = GETDATE();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time, @end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.erp_loc_a101';
+		truncate table bronze.erp_loc_a101;
+
+		print '>> Inserting Table : bronze.erp_loc_a101';
+		bulk insert bronze.erp_loc_a101
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = GETDATE();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time, @end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		set @start_time = GETDATE();
+		print '>> Truncating Table : bronze.erp_px_cat_g1v2';
+		truncate table bronze.erp_px_cat_g1v2;
+
+		print '>> Insterting Table : bronze.erp_px_cat_g1v2';
+		bulk insert bronze.erp_px_cat_g1v2
+		from 'C:\Users\roshn\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		with (
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+		set @end_time = GETDATE();
+		print '>> Load Duration: '+ cast(datediff(second, @start_time, @end_time)as nvarchar)+ 'seconds';
+		print '>> ----------------------------';
+
+		set @batch_end_time=getdate();
+		print '==================================================';
+		print 'Loading Bronze Layer is Completed';
+		print '   - Total Load Duration: ' + cast(datediff (second, @batch_start_time , @batch_end_time)as nvarchar)+ ' seconds';
+		print '==================================================';
+	end try
+	begin catch
+		print '================================================================';
+		print 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		print '================================================================';
+	end catch
+
+end
